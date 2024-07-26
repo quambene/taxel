f38da775c950ee28703e1efeec929fd211a8104e
quambene <contact.quambene@gmail.com>
2024-07-26 03:27:32 +0200

    Generate ebilanz

--- taxel-py/src/xsd_to_xml.py
+++ taxel-py/src/xsd_to_xml.py
@@ -1,22 +1,31 @@
 import pytest
 from lxml import etree
-from xmlschema import XMLSchema, XMLSchemaValidationError
+from xmlschema import XMLSchema10, XMLSchemaValidationError
 import json
 import xml.etree.ElementTree as ET
 
 
-def generate_xml(schema_path: str, data_path: str) -> ET.ElementTree:
+def generate_xml(schema_path: str, data_path: str, target_namespace: str | None, namespaces: dict[str, str] | None) -> ET.ElementTree:
     print("Generate xml from schema")
 
     # Load and parse the XSD file
-    schema = XMLSchema(schema_path)
+    schema = XMLSchema10(schema_path, loglevel=20, validation='strict')
+
+    if namespaces is not None:
+        if target_namespace is not None:
+            namespaces[target_namespace] = schema.target_namespace
 
     json_file = open(data_path, 'r')
     json_data = json_file.read()
     json_data = json.loads(json_data)
     print(json_data)
 
-    xml_data = schema.encode(json_data)
+    if namespaces is None:
+        xml_data = schema.encode(
+            json_data, validation='strict')
+    else:
+        xml_data = schema.encode(
+            json_data, validation='strict', preserve_root=True, namespaces=namespaces)
 
     if isinstance(xml_data, ET.Element):
         xml_tree = ET.ElementTree(xml_data)
@@ -35,16 +44,39 @@ def generate_xml(schema_path: str, data_path: str) -> ET.ElementTree:
 
 @pytest.mark.unit
 def test_generate_xml_simple():
-    schema_path = '../test_data/schema/simple/schema.xsd'
-    data_path = '../test_data/schema/simple/data.json'
+    schema_path = '../test_data/simple/schema.xsd'
+    input_path = '../test_data/simple/input.json'
+    output_path = '../test_data/simple/output.xml'
 
-    xml = generate_xml(schema_path, data_path)
+    xml = generate_xml(schema_path, input_path, None, None)
 
     root = xml.getroot()
     actual_xml = ET.tostring(root, encoding='utf-8', xml_declaration=True)
 
-    expected_path = '../test_data/schema/simple/generated.xml'
-    with open(expected_path, 'r', encoding='utf-8') as file:
+    with open(output_path, 'r', encoding='utf-8') as file:
         expected_xml = file.read()
 
     assert actual_xml.decode("utf-8") == expected_xml
+
+
+@pytest.mark.unit
+def test_generate_ebilanz():
+    schema_path = '../test_data/ebilanz/schema/ebilanz_000002.xsd'
+    input_path = '../test_data/ebilanz/schema/input.json'
+    output_path = '../test_data/ebilanz/schema/output.xml'
+    target_namespace = 'ebilanz'
+    namespaces = {
+        'xbrli': "http://www.xbrl.org/2003/instance"}
+
+    xml = generate_xml(schema_path, input_path, target_namespace, namespaces)
+
+    root = xml.getroot()
+    actual_xml = ET.tostring(root, encoding='utf-8', xml_declaration=True)
+
+    with open(output_path, 'wb') as file:
+        xml.write(file, encoding='utf-8', xml_declaration=True)
+
+    # with open(output_path, 'r', encoding='utf-8') as file:
+    #     expected_xml = file.read()
+
+    # assert actual_xml.decode("utf-8") == expected_xml
