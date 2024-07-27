3bd3f6a87aaa614bb62e7f6c446e90d474648b83
quambene <contact.quambene@gmail.com>
2024-07-28 00:44:36 +0200

    Generate ebilanz

--- taxel-py/src/xsd_to_xml.py
+++ taxel-py/src/xsd_to_xml.py
@@ -25,7 +25,7 @@ def generate_xml(schema_path: str, data_path: str, target_namespace: str | None,
             json_data, validation='strict')
     else:
         xml_data = schema.encode(
-            json_data, validation='strict', preserve_root=True, namespaces=namespaces)
+            json_data, validation='strict', preserve_root=True, unordered=True, namespaces=namespaces)
 
     if isinstance(xml_data, ET.Element):
         xml_tree = ET.ElementTree(xml_data)
@@ -42,6 +42,21 @@ def generate_xml(schema_path: str, data_path: str, target_namespace: str | None,
     return xml_tree
 
 
+namespaces = {
+    "xsi": "http://www.w3.org/2001/XMLSchema-instance",
+    "xlink": "http://www.w3.org/1999/xlink",
+    "xhtml": "http://www.w3.org/1999/xhtml",
+    "xbrli": "http://www.xbrl.org/2003/instance",
+    "xbrldi": "http://xbrl.org/2006/xbrldi",
+    "link": "http://www.xbrl.org/2003/linkbase",
+    "iso4217": "http://www.xbrl.org/2003/iso4217",
+    # taxonomy v6.6 for Global Common Document (GCD)
+    "de-gcd": "http://www.xbrl.de/taxonomies/de-gcd-2022-05-02",
+    # taxonomy v6.6 for Generally Accepted Accounting Principles (GAAP)
+    "de-gaap-ci": "http://www.xbrl.de/taxonomies/de-gaap-ci-2022-05-02"
+}
+
+
 @pytest.mark.unit
 def test_generate_xml_simple():
     schema_path = '../test_data/simple/schema.xsd'
@@ -49,7 +64,6 @@ def test_generate_xml_simple():
     output_path = '../test_data/simple/output.xml'
 
     xml = generate_xml(schema_path, input_path, None, None)
-
     root = xml.getroot()
     actual_xml = ET.tostring(root, encoding='utf-8', xml_declaration=True)
 
@@ -65,18 +79,12 @@ def test_generate_ebilanz():
     input_path = '../test_data/ebilanz/schema/input.json'
     output_path = '../test_data/ebilanz/schema/output.xml'
     target_namespace = 'ebilanz'
-    namespaces = {
-        'xbrli': "http://www.xbrl.org/2003/instance"}
 
     xml = generate_xml(schema_path, input_path, target_namespace, namespaces)
-
     root = xml.getroot()
     actual_xml = ET.tostring(root, encoding='utf-8', xml_declaration=True)
 
-    with open(output_path, 'wb') as file:
-        xml.write(file, encoding='utf-8', xml_declaration=True)
+    with open(output_path, 'r', encoding='utf-8') as file:
+        expected_xml = file.read()
 
-    # with open(output_path, 'r', encoding='utf-8') as file:
-    #     expected_xml = file.read()
-
-    # assert actual_xml.decode("utf-8") == expected_xml
+    assert actual_xml.decode("utf-8") == expected_xml
