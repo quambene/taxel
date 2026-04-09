2eb227aab8f2d56942079233c3ac094312ee84bb
quambene <contact.quambene@gmail.com>
2026-04-09 05:18:24 +0200

    Persist app settings (#16)

--- taxel-gui/src/main.rs
+++ taxel-gui/src/main.rs
@@ -4,14 +4,16 @@ mod widgets;
 use crate::app::TaxelApp;
 use eframe::egui::{ViewportBuilder, Visuals};
 use log::debug;
-
 fn main() -> Result<(), anyhow::Error> {
     // Use hot reload in debug mode
     #[cfg(debug_assertions)]
     dioxus_devtools::connect_subsecond();
 
+    let persistence_path = dirs::config_dir().map(|path| path.join("taxel").join("settings.ron"));
+
     let options = eframe::NativeOptions {
         viewport: ViewportBuilder::default().with_maximized(true),
+        persistence_path,
         ..Default::default()
     };
 
@@ -22,7 +24,7 @@ fn main() -> Result<(), anyhow::Error> {
         options,
         Box::new(|ctx| {
             ctx.egui_ctx.set_visuals(Visuals::light());
-            Ok(Box::new(TaxelApp::new(None, None)))
+            Ok(Box::new(TaxelApp::new(ctx, None, None)))
         }),
     )
     .map_err(|err| anyhow::anyhow!(err.to_string()))?;
