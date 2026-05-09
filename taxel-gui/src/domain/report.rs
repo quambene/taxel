quambene/taxel

commit 9335ee6069bafeeac8b820cce213a4f9f7945e7d

fix: import values of selected report sections only

taxel-gui/src/domain/report.rs
@@ -420,6 +420,15 @@ impl Report {
 
         for source_section in &source_report.sections {
             for row in &source_section.rows {
+                // Skip reportElements: import values of selected report
+                // sections but don't change the report structure.
+                if row
+                    .concept
+                    .starts_with("genInfo.report.id.reportElement.reportElements.")
+                {
+                    continue;
+                }
+
                 let source_value = match &row.value {
                     FactValue::Text(text) => {
                         let is_nil = row

commit 350fa54a930ad6adf8d5be29ec173e65663d8974

fix: revert regression from 9335ee6

taxel-gui/src/domain/report.rs
@@ -248,6 +248,7 @@ impl Report {
             if section.role == GCD_ROLE_URI {
                 continue;
             }
+
             if let Some(&enabled) = enabled_roles.get(section.role.as_str()) {
                 section.disabled = !enabled;
             }
@@ -420,15 +421,6 @@ impl Report {
 
         for source_section in &source_report.sections {
             for row in &source_section.rows {
-                // Skip reportElements: import values of selected report
-                // sections but don't change the report structure.
-                if row
-                    .concept
-                    .starts_with("genInfo.report.id.reportElement.reportElements.")
-                {
-                    continue;
-                }
-
                 let source_value = match &row.value {
                     FactValue::Text(text) => {
                         let is_nil = row